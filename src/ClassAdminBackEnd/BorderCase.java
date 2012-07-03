958a5cfe1c9853e5969a4c7ec538ac8235737f51
wendersnawen <gerhard@gerhard>
2012-07-03 14:23:01 +0200

    added isBorderCase

--- src/ClassAdminBackEnd/BorderCase.java
+++ src/ClassAdminBackEnd/BorderCase.java
@@ -3,4 +3,16 @@ package ClassAdminBackEnd;
 public class BorderCase {
 	Double lowVal;
 	Double highVal;
+	
+	/**
+	 * @param markE
+	 * @return
+	 */
+	public boolean isBorderCase(MarkEntity markE){
+		if((markE.mark < highVal) && (markE.mark > lowVal)){
+			return true;
+		}
+		
+		return false;
+	}
 }
