f946211d6e616db8f390075202963cf09400dbe3
wendersnawen <gerhard@gerhard>
2012-07-03 15:05:25 +0200

    made variable private and added getteters and setters

--- src/ClassAdminBackEnd/SpreadsheetView.java
+++ src/ClassAdminBackEnd/SpreadsheetView.java
@@ -1,8 +1,16 @@
 package ClassAdminBackEnd;
 
 public class SpreadsheetView {
-	MarkEntity[][] markEntity2DArray;
+	private MarkEntity[][] markEntity2DArray;
 	
+	public MarkEntity[][] getMarkEntity2DArray() {
+		return markEntity2DArray;
+	}
+
+	public void setMarkEntity2DArray(MarkEntity[][] markEntity2DArray) {
+		this.markEntity2DArray = markEntity2DArray;
+	}
+
 	public void draw(){
 		
 		return;
