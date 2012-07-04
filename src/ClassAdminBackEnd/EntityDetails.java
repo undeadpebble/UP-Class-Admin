d3751e4f85469199196d8a7f2dec6c43145cca12
undeadpebble <undeadpebble@undeadpebble-PC>
2012-07-04 12:05:54 +0200

    EntityDetails encapsulation

--- src/ClassAdminBackEnd/EntityDetails.java
+++ src/ClassAdminBackEnd/EntityDetails.java
@@ -5,10 +5,47 @@ import java.awt.Image;
 import java.util.LinkedList;
 
 public class EntityDetails {
-	EntityType type;
-	LinkedList<String> fields;
-	Image Picture;
-	Color highlightColor;
-	Color textColor;
-	Boolean absentExcuse;
+	private EntityType type;
+	private LinkedList<String> fields;
+	private Image Picture;
+	private Color highlightColor;
+	private Color textColor;
+	private Boolean absentExcuse;
+	public EntityType getType() {
+		return type;
+	}
+	public void setType(EntityType type) {
+		this.type = type;
+	}
+	public LinkedList<String> getFields() {
+		return fields;
+	}
+	public void setFields(LinkedList<String> fields) {
+		this.fields = fields;
+	}
+	public Image getPicture() {
+		return Picture;
+	}
+	public void setPicture(Image picture) {
+		Picture = picture;
+	}
+	public Color getHighlightColor() {
+		return highlightColor;
+	}
+	public void setHighlightColor(Color highlightColor) {
+		this.highlightColor = highlightColor;
+	}
+	public Color getTextColor() {
+		return textColor;
+	}
+	public void setTextColor(Color textColor) {
+		this.textColor = textColor;
+	}
+	public Boolean getAbsentExcuse() {
+		return absentExcuse;
+	}
+	public void setAbsentExcuse(Boolean absentExcuse) {
+		this.absentExcuse = absentExcuse;
+	}
+	
 }
