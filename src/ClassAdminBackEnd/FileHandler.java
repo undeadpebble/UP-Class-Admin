f34739b9140c3e35c867ba6989a8849aed82c62e
undeadpebble <undeadpebble@undeadpebble-PC>
2012-07-04 15:16:35 +0200

    FileHandler started on

--- src/ClassAdminBackEnd/FileHandler.java
+++ src/ClassAdminBackEnd/FileHandler.java
@@ -1,5 +1,17 @@
 package ClassAdminBackEnd;
 
 public class FileHandler {
-
+	public void openFile(String filename) throws UnsupportedFileTypeException{
+		CsvImport fileReader;
+		if(filename.substring(filename.indexOf('.')).contains("csv")){
+			fileReader = new CsvImport();
+		}
+		else
+			throw new UnsupportedFileTypeException();
+		
+		
+		if(fileReader.fileExists(filename)){
+			//TODO 
+		}
+	}
 }
---
+++ src/ClassAdminBackEnd/UnsupportedFileTypeException.java
@@ -0,0 +1,14 @@
+package ClassAdminBackEnd;
+
+public class UnsupportedFileTypeException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedFileTypeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
