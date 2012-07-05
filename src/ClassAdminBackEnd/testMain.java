3b3523fdecabf9242147336ddf636655d5b181a5
undeadpebble <undeadpebble@192.168.2.9>
2012-07-05 15:32:21 +0200

    backend- read file into local memory

--- src/ClassAdminBackEnd/testMain.java
+++ src/ClassAdminBackEnd/testMain.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 
 public class testMain
 {
+	
 	public static void main(String[] args) 
 	{
-		CsvImport c = new CsvImport();
+		FileHandler fHandler = new FileHandler();
+		try{
+		fHandler.openFile("test.csv");
+		}
+		catch(UnsupportedFileTypeException e){
+			System.out.println("Unsupported filetype");
+		}
+		/*CsvImport c = new CsvImport();
 		if(c.fileExists("test.csv"))
 		{
 			ArrayList csv =  c.recordData();
@@ -21,7 +29,26 @@ public class testMain
 			System.out.println(c.getRecordFieldValue(csv,0 , 0));
 			
 		
+		}*/
+		
+		Global g = Global.getGlobal();
+		MarkEntity head = Global.getGlobal().getActiveProject().getHead();
+		for (int i = 0;i<head.getSubEntity().size();++i){
+			try{
+			System.out.println(i+" "+head.getSubEntity().get(i).getDetails().getFields().get(0));
+			}
+			catch(IndexOutOfBoundsException e){
+				System.out.println(i+" "+head.getSubEntity().get(i).getMark());
+			}
+			for (int j = 0;j<head.getSubEntity().get(i).getSubEntity().size();++j){
+				try {
+					System.out.println(j+" "+head.getSubEntity().get(i).getSubEntity().get(j).getDetails().getFields().get(0));
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println(j+" "+head.getSubEntity().get(i).getSubEntity().get(j).getMark());
+				}
+			}
 		}
+		System.out.println();
 		
 	}
 }
