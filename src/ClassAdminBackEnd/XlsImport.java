6f09a73e71c4297939f2a104bf90206d8daf0ce6
godforbidd <marco@demoteam.co.za>
2012-07-30 13:38:57 +0200

    xlsImporter done

--- src/ClassAdminBackEnd/XlsImport.java
+++ src/ClassAdminBackEnd/XlsImport.java
@@ -17,52 +17,78 @@ public class XlsImport {
 	File reader;
 	Workbook w;
 	Sheet sheet;
+	int headerLine = 0;
 
 	public XlsImport() {
 
 	}
 
 	public boolean fileExists(String in) {
-		try 
-		{
+		try {
 			reader = new File(in);
-			System.out.println("File found");
-			w = Workbook.getWorkbook(reader);
-			System.out.println("FAIL");
-			
-			
-			//createWorkbook(reader);
+			createWorkbook(reader);
 			return true;
-		} 
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
-	private void createWorkbook(File f) throws IOException
-	{
+
+	private void createWorkbook(File f) throws IOException {
 		try {
 			w = Workbook.getWorkbook(f);
 		} catch (BiffException e) {
 			System.out.println("1");
 			e.printStackTrace();
-		} 
+		} catch (IOException e) {
+			System.out.println("2");
+			e.printStackTrace();
+		}
 	}
+
 	public void setSheet(int s) {
 		sheet = w.getSheet(s);
 	}
 
-	public int getLines() {
-		return 0;
+	public void setHeaderLine(int s) {
+		headerLine = s;
 	}
 
-	public void recordData() {
+	public int getLines() {
+		return sheet.getRows();
+	}
+
+	public ArrayList recordData() {
 		ArrayList data = new ArrayList();
 		ArrayList headers = null;
 		ArrayList records = null;
-		ArrayList record = null;
+
+		headers = new ArrayList();// get headers
+		for (int i = 0; i < sheet.getColumns(); i++) {
+			Cell cell = sheet.getCell(i, headerLine);
+			headers.add(cell.getContents());
+			// System.out.println(cell.getContents());
+			cell = null;
+		}
+		data.add(headers);
+
+		records = new ArrayList(); // get records
+		for (int j = headerLine + 1; j < sheet.getRows(); j++) {
+			ArrayList record = new ArrayList();
+			for (int i = 0; i < sheet.getColumns(); i++) {
+				Cell cell = sheet.getCell(i, j);
+				String pp = cell.getContents();
+				record.add(pp);
+				//System.out.print(pp + '\t');
+			}
+			records.add(record);
+			record = null;
+			 //System.out.println();
+		}
+		data.add(records);
+		records = null;
+
+		return data;
 	}
 
 	public void printSheet() {
@@ -73,5 +99,111 @@ public class XlsImport {
 			}
 			System.out.println();
 		}
+	}
+
+	public ArrayList getHeaders(ArrayList arr) // return header arraylist from
+	// arraylist
+	{
+		ArrayList headers = (ArrayList) arr.get(0);
+		return headers;
+	}
+
+	public ArrayList getRecords(ArrayList arr) // return record arraylist from
+	// arraylist
+	{
+		ArrayList records = (ArrayList) arr.get(1);
+		return records;
+	}
+
+	public ArrayList getRecord(ArrayList arr, int index) // return specified
+	// record from
+	// arraylist
+	{
+		ArrayList records = (ArrayList) arr.get(1); // get records arraylist
+		ArrayList record = (ArrayList) records.get(index); // get specified
+		// record in record
+		// arraylist
+		return record;
+	}
+
+	public String getRecordFieldValue(ArrayList arr, int recordIndex,
+			int fieldIndex) // return specified field from record in file
+	{
+		ArrayList records = (ArrayList) arr.get(1); // get records arraylist
+		ArrayList record = (ArrayList) records.get(recordIndex); // get
+		// specified
+		// record in
+		// records
+		// arraylist
+		String field = (String) record.get(fieldIndex); // get specified field
+		// in record
+		return field;
+	}
+
+	public void printHeaders(ArrayList headers) // print all headers from
+	// headers arraylist
+	{
+		for (int j = 0; j < headers.size(); j++) {
+			System.out.print(headers.get(j).toString() + "\t");
+		}
+		System.out.println();
+	}
+
+	public void printRecords(ArrayList records) // print all records from
+	// records arraylist
+	{
+		for (int i = 0; i < records.size(); i++) {
+			ArrayList record = (ArrayList) records.get(i); // get record
+			// arraylist from
+			// records
+
+			for (int j = 0; j < record.size(); j++) // get field from record
+			// arraylist
+			{
+				System.out.print(record.get(j).toString() + "\t"); // print
+				// field
+				// data
+			}
+			System.out.println(); // new line for next record
+
+		}
+	}
+
+	public void printRecord(ArrayList record) // print all information from
+	// specified record
+	{
+		for (int j = 0; j < record.size(); j++) {
+			System.out.print(record.get(j).toString() + "\t"); // print field
+			// data
+		}
+		System.out.println(); // new line XD
+	}
+
+	public void print(ArrayList in) // print entire structure
+	{
+
+		ArrayList headers = (ArrayList) in.get(0); // get header arraylist
+		ArrayList records = (ArrayList) in.get(1); // get records arraylist
+
+		for (int j = 0; j < headers.size(); j++) // print all headers on header
+		// arraylist
+		{
+			System.out.print(headers.get(j).toString() + "\t");
+		}
+		System.out.println();
+
+		for (int i = 0; i < records.size(); i++) // get each record arraylist in
+		// records arraylist
+		{
+			ArrayList record = (ArrayList) records.get(i); // specify record
+
+			for (int j = 0; j < record.size(); j++) // print all field data from
+			// record arraylist
+			{
+				System.out.print(record.get(j).toString() + "\t");
+			}
+			System.out.println(); // next record to follow
+
+		}// get records
 	}
 }
--- src/ClassAdminBackEnd/testMain.java
+++ src/ClassAdminBackEnd/testMain.java
@@ -10,36 +10,33 @@ public class testMain
 	
 	public static void main(String[] args) 
 	{
-		FileHandler fh = new FileHandler();
+		XlsImport importer = new XlsImport();
+		
+		importer.fileExists("C:/Users/Marco/Workspace/UP-Class-Admin/book1.xls");
+		importer.setSheet(0);
+		importer.setHeaderLine(0);
+		ArrayList arr = importer.recordData();
+		importer.print(arr);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		CsvImport csv = new CsvImport();
+		
+		csv.fileExists("C:/Users/Marco/Workspace/UP-Class-Admin/test.csv");
+		ArrayList arr2 = csv.recordData();
+		csv.print(arr2);
+		
+		/*FileHandler fh = new FileHandler();
 		
 		try {
 			fh.openFile("test.csv");
 		} catch (UnsupportedFileTypeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		Project p = Global.getGlobal().getActiveProject();
-		p.getEntityTypes().get(0).getBorderCasing().add(new BorderCase(5.0,10.0));
-		p.getEntityTypes().get(0).getFormatting().add(new Format(0,0,1.0,2.0,new Color(1.0f,0.2f,0.3f),new Color(1.0f,0.2f,0.3f),"test conditional formatting"));
-		p.getEntityTypes().get(0).getFields().add("test");
-		p.getEntityTypes().get(0).getFields().add("test1");
-		p.getEntityTypes().get(0).getFields().add("test2");
-		p.getEntityTypes().get(0).getFieldDefaults().add("test");
-		p.getEntityTypes().get(0).getFieldDefaults().add("test2");
-		p.getEntityTypes().get(0).getFieldDefaults().add("test3");
-		p.getEntityTypes().get(0).setDate(new Date());
-		Boolean[] b = {true,true,false};
-		p.getEntityTypes().get(0).setVisibleFields(b);
-		p.getEntityTypes().get(0).getEntityList().get(0).getDetails().getFields().add("test5");
-		p.getEntityTypes().get(0).getEntityList().get(0).getDetails().getFields().add("test6");
-		p.getEntityTypes().get(0).getEntityList().get(0).getDetails().getFields().add("test7");
-		pdatImport PI = new pdatImport();
-		String str = Global.getGlobal().getActiveProject().toString();
-		PI.write(str.getBytes(), "test.pdat");
-		System.out.println(Global.getGlobal().getActiveProject());
-		PI.read("test.pdat");
-		System.out.println(Global.getGlobal().getActiveProject());
-		
+		}*/
 		
 	}
 }
