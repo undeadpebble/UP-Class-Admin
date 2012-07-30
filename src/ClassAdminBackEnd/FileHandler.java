a5269cae6b70737c7182a6e1d4097519a2c50af9
undeadpebble <undeadpebble@10.0.0.6>
2012-07-30 19:15:01 +0200

    created fileImport superclass

--- src/ClassAdminBackEnd/FileHandler.java
+++ src/ClassAdminBackEnd/FileHandler.java
@@ -7,95 +7,125 @@ public class FileHandler {
 	 * @param str
 	 * @return
 	 */
+	static FileHandler fh;
+
+	public static FileHandler get() {
+		if (fh == null)
+			fh = new FileHandler();
+		return fh;
+	}
+
+	private FileHandler() {
+
+	}
 
 	public void openFile(String filename) throws UnsupportedFileTypeException {
-		
-		Global glob = Global.getGlobal();
+
 		if (filename.substring(filename.indexOf('.')).contains("csv")) {
-			CsvImport fileReader;
-			fileReader = new CsvImport();
-			ArrayList headers;
-			String record;
-			EntityTypeFactory eTFactory = new EntityTypeFactory();
-			MarkEntityFactory mEFactory = new MarkEntityFactory();
-			glob.getActiveProject().getEntityTypes().clear();
-			eTFactory.makeEntityTypeCSV("File", true);
+			openCSV(filename);
+		} else if (filename.substring(filename.indexOf('.')).contains("pdat")) {
+			pdatImport PI = new pdatImport();
+			PI.read(filename);
 
-			MarkEntity mE = mEFactory.makeEntity(glob.getActiveProject()
-					.getEntityTypes().get(0), null);
-			glob.getActiveProject().setHead(mE);
-
-			if (fileReader.fileExists(filename)) {
-				ArrayList csv = fileReader.recordData();
-				headers = fileReader.getHeaders(csv);
-				record = fileReader.getRecordFieldValue(csv, 0, 0);
-				
-				// create entity types
-				for (int i = 0; i < headers.size(); ++i) {
-					record = fileReader.getRecordFieldValue(csv, 0, i);
-					try {
-						Integer.parseInt(record);
-						if (record.length() > 4) {
-							eTFactory.makeEntityTypeCSV((String) headers.get(i),
-									true).getFields().add((String) headers.get(i));
-
-						} else {
-							eTFactory.makeEntityTypeCSV((String) headers.get(i),
-									false);
-
-						}
-					} catch (NumberFormatException e) {
-						eTFactory.makeEntityTypeCSV((String) headers.get(i), true).getFields().add((String) headers.get(i));
-
-					}
-
-				}
-				// create MarkEntities
-				int numRecords = fileReader.getRecords(csv).size();
-				for (int r = 0; r < numRecords; ++r) {
-					// make parent for row
-					MarkEntity parent = mEFactory.makeEntity(glob
-							.getActiveProject().getEntityTypes().get(1), glob
-							.getActiveProject().getHead());
-					record = fileReader.getRecordFieldValue(csv, r, 0);
-					if (glob.getActiveProject().getEntityTypes().get(1)
-							.getIsTextField() == true) {
-						parent.getDetails().getFields().add(record);
-					} else {
-						try {
-							parent.setMark(Integer.parseInt(record));
-						} catch (NumberFormatException e) {
-							parent.setMark(0);
-						}
-					}
-
-					for (int f = 1; f < headers.size(); ++f) {
-						record = fileReader.getRecordFieldValue(csv, r, f);
-						EntityType fieldType = glob.getActiveProject()
-								.getEntityTypes().get(f + 1);
-						mE = mEFactory.makeEntity(fieldType, parent);
-						if (fieldType.getIsTextField() == true) {
-							mE.getDetails().getFields().add(record);
-						} else {
-							try {
-								mE.setMark(Integer.parseInt(record));
-							} catch (NumberFormatException e) {
-								mE.setMark(0);
-							}
-						}
-					}
-				}
-			}
-		} else 
-			if(filename.substring(filename.indexOf('.')).contains("pdat")){
-				pdatImport PI = new pdatImport();
-				PI.read(filename);
-			
-			}
-			else
+		} else
 			throw new UnsupportedFileTypeException();
 
-		
-	
 	}
+
+	private void openCSV(String filename) {
+
+		FileImport fileReader;
+		fileReader = new CsvImport();
+		ArrayList headers;
+
+		if (fileReader.fileExists(filename)) {
+			ArrayList recordArray = fileReader.recordData();
+			headers = fileReader.getHeaders(recordArray);
+			createEntitieTypes(headers, recordArray, fileReader);
+			createMarkEntities(headers, recordArray, fileReader);
+		}
+	}
+
+	private void createEntitieTypes(ArrayList headers, ArrayList recordArray,
+			FileImport fileReader) {
+		// header entity
+		Global glob = Global.getGlobal();
+		EntityTypeFactory eTFactory = new EntityTypeFactory();
+		MarkEntityFactory mEFactory = new MarkEntityFactory();
+		glob.getActiveProject().getEntityTypes().clear();
+		eTFactory.makeEntityTypeFileImport("File", true);
+
+		MarkEntity mE = mEFactory.makeEntity(glob.getActiveProject()
+				.getEntityTypes().get(0), null);
+		glob.getActiveProject().setHead(mE);
+		// create entity types
+		for (int i = 0; i < headers.size(); ++i) {
+			String record = fileReader.getRecordFieldValue(recordArray, 0, i);
+			try {
+				Integer.parseInt(record);
+				if (record.length() > 4) {
+					eTFactory
+							.makeEntityTypeFileImport((String) headers.get(i),
+									true).getFields()
+							.add((String) headers.get(i));
+
+				} else {
+					eTFactory.makeEntityTypeFileImport((String) headers.get(i),
+							false);
+
+				}
+			} catch (NumberFormatException e) {
+				eTFactory
+						.makeEntityTypeFileImport((String) headers.get(i), true)
+						.getFields().add((String) headers.get(i));
+
+			}
+
+		}
+	}
+
+	private void createMarkEntities(ArrayList headers, ArrayList recordArray,
+			FileImport fileReader) {
+		Global glob = Global.getGlobal();
+		EntityTypeFactory eTFactory = new EntityTypeFactory();
+		MarkEntityFactory mEFactory = new MarkEntityFactory();
+
+		// create MarkEntities
+		int numRecords = fileReader.getRecords(recordArray).size();
+		for (int r = 0; r < numRecords; ++r) {
+			// make parent for row
+			MarkEntity parent = mEFactory
+					.makeEntity(
+							glob.getActiveProject().getEntityTypes().get(1),
+							glob.getActiveProject().getHead());
+			String record = fileReader.getRecordFieldValue(recordArray, r, 0);
+			if (glob.getActiveProject().getEntityTypes().get(1)
+					.getIsTextField() == true) {
+				parent.getDetails().getFields().add(record);
+			} else {
+				try {
+					parent.setMark(Integer.parseInt(record));
+				} catch (NumberFormatException e) {
+					parent.setMark(0);
+				}
+			}
+
+			for (int f = 1; f < headers.size(); ++f) {
+				record = fileReader.getRecordFieldValue(recordArray, r, f);
+				EntityType fieldType = glob.getActiveProject().getEntityTypes()
+						.get(f + 1);
+				MarkEntity mE = mEFactory.makeEntity(fieldType, parent);
+				if (fieldType.getIsTextField() == true) {
+					mE.getDetails().getFields().add(record);
+				} else {
+					try {
+						mE.setMark(Integer.parseInt(record));
+					} catch (NumberFormatException e) {
+						mE.setMark(0);
+					}
+				}
+			}
+		}
+	}
+
 }
---
+++ src/ClassAdminBackEnd/FileImport.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ClassAdminBackEnd;
+
+import java.util.ArrayList;
+
+/**
+ * @author undeadpebble
+ *
+ */
+public class FileImport {
+	public Boolean fileExists(String filename){
+		
+		return false;
+	}
+	
+	public ArrayList recordData(){
+		
+		return null;
+	}
+	
+	public ArrayList getHeaders(ArrayList arr){
+		return null;
+	}
+	
+	public ArrayList getRecords(ArrayList arr){
+		return null;
+	}
+	
+	public String getRecordFieldValue(ArrayList records,int r,int c){
+		return null;
+	}
+}
