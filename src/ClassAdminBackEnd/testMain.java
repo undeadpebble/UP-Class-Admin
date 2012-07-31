b6b90c705cf1ef2cfb0d5f8e8e395fb31823297b
godforbidd <marco@demoteam.co.za>
2012-07-31 11:22:27 +0200

    fixed no sheet or headerline selected in xlsImport

--- src/ClassAdminBackEnd/testMain.java
+++ src/ClassAdminBackEnd/testMain.java
@@ -4,14 +4,37 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 import jxl.*;
 public class testMain
 {
 	
 	public static void main(String[] args) 
 	{
+/*		XlsImport x = new XlsImport();
+		if(x.fileExists("book1.xls"))
+		{
+			ArrayList arr = x.recordData();
+			x.printHeaders(x.getHeaders(arr));
+			x.printRecords(x.getRecords(arr));
+			x.print(arr);
+		}
+*/		
+		CsvImport c = new CsvImport();
+		{
+			if(c.fileExists("test.csv"))
+			{
+				ArrayList arr = c.recordData();
+
+				c.printHeaders(c.getHeaders(arr));
+				c.printRecords(c.getRecords(arr));
+				c.print(arr);
+			}
+			
+		}
 		
-		FileHandler fh = FileHandler.get();
+/*		FileHandler fh = FileHandler.get();
 		
 		try {
 			fh.openFile("book1.xls");
@@ -20,6 +43,6 @@ public class testMain
 			e.printStackTrace();
 		}
 		System.out.println(Global.getGlobal().getActiveProject().toString());
-		
+*/		
 	}
 }
