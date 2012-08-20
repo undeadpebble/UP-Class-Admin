f67ccfabb413339cf09e2e842a6e1a1297f448b7
user <user@137.215.37.203>
2012-08-20 11:35:53 +0200

    working on add button

--- src/ClassAdminFrontEnd/FrmTable.java
+++ src/ClassAdminFrontEnd/FrmTable.java
@@ -140,8 +140,6 @@ public class FrmTable extends JPanel {
 		};
 		table.setAutoCreateRowSorter(true);
 		
-		
-
 		TableCellListener tcl = new TableCellListener(table, action);
 
 		table.addPropertyChangeListener(tcl);
