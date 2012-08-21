1b4f3fbe3996d4c36c526e9988d999b5d6fecd67
user <user@137.215.37.215>
2012-08-21 10:29:34 +0200

    b

--- src/ClassAdminBackEnd/testMain.java
+++ src/ClassAdminBackEnd/testMain.java
@@ -3,18 +3,12 @@ package ClassAdminBackEnd;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Date;
-
-<<<<<<< HEAD
-=======
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
->>>>>>> refs/remotes/origin/dev
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
-<<<<<<< HEAD
-=======
 
 
 
@@ -28,7 +22,6 @@ import ClassAdminFrontEnd.Frame;
 
 import ClassAdminFrontEnd.FrmTable;
 import ClassAdminFrontEnd.TreeView;
->>>>>>> refs/remotes/origin/dev
 
 import jxl.*;
 public class testMain
@@ -36,73 +29,13 @@ public class testMain
 	
 	public static void main(String[] args) 
 	{
-/*		XlsImport x = new XlsImport();
-		if(x.fileExists("book1.xls"))
-		{
-			ArrayList arr = x.recordData();
-			x.printHeaders(x.getHeaders(arr));
-			x.printRecords(x.getRecords(arr));
-			x.print(arr);
-		}
-*/		
-		CsvImport c = new CsvImport();
-		{
-			if(c.fileExists("test.csv"))
-			{
-				ArrayList arr = c.recordData();
-
-				c.printHeaders(c.getHeaders(arr));
-				c.printRecords(c.getRecords(arr));
-				c.print(arr);
-			}
-			
-		}
-		
-/*		FileHandler fh = FileHandler.get();
-		
-		try {
-			fh.openFile("book1.xls");
-		} catch (UnsupportedFileTypeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-<<<<<<< HEAD
-		System.out.println(Global.getGlobal().getActiveProject().toString());
-*/		
-=======
-		//System.out.println(Global.getGlobal().getActiveProject().toString());
-
-		PDatExport test = new PDatExport();
-		try {
-			test.exportFile(Global.getGlobal().getActiveProject(), "test.pdat");
-		} catch (SqlJetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-		
-		
 		
 		JFrame fr = new JFrame();
 		
 		fr.setVisible(true);
 		fr.setSize(600, 600);
 		
-		/*FrmTable tble = new FrmTable(Global.getGlobal().getActiveProject().getHead().getHeaders(), Global.getGlobal().getActiveProject().getHead().getDataLinkedList(), Global.getGlobal().getActiveProject());
-		
-		fr.add(tble);
-		fr.repaint();*/
 		EntityTypePanel temp = new EntityTypePanel(Global.getGlobal().getActiveProject());
 		fr.add(temp);
-		
-
-
-		
-
-		
-		
-
->>>>>>> refs/remotes/origin/dev
 	}
 }
