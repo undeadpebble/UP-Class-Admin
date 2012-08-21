947c57867dca2562e576ef971025845d4afd7aad
user <user@137.215.37.215>
2012-08-21 10:21:43 +0200

    b

--- src/ClassAdminBackEnd/testMain.java
+++ src/ClassAdminBackEnd/testMain.java
@@ -4,13 +4,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Date;
 
-import javax.swing.JComponent;
-import javax.swing.JFrame;
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
-
-import ClassAdminFrontEnd.Frame;
-import ClassAdminFrontEnd.FrmTable;
-import ClassAdminFrontEnd.TreeView;
 
 import jxl.*;
 public class testMain
@@ -26,51 +20,29 @@ public class testMain
 			x.printRecords(x.getRecords(arr));
 			x.print(arr);
 		}
-		
+*/		
 		CsvImport c = new CsvImport();
 		{
 			if(c.fileExists("test.csv"))
 			{
 				ArrayList arr = c.recordData();
-=======
->>>>>>> refs/remotes/origin/louis
 
-<<<<<<< HEAD
 				c.printHeaders(c.getHeaders(arr));
 				c.printRecords(c.getRecords(arr));
 				c.print(arr);
 			}
 			
-		}*/
-
+		}
 		
-		FileHandler fh = FileHandler.get();
+/*		FileHandler fh = FileHandler.get();
 		
 		try {
-
-			fh.openFile("test.csv");
-
+			fh.openFile("book1.xls");
 		} catch (UnsupportedFileTypeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//System.out.println(Global.getGlobal().getActiveProject().toString());
-		
-		
-		
-		JFrame fr = new JFrame();
-		
-		fr.setVisible(true);
-		fr.setSize(600, 600);
-		
-		FrmTable tble = new FrmTable(Global.getGlobal().getActiveProject().getHead().getHeaders(), Global.getGlobal().getActiveProject().getHead().getDataLinkedList(), Global.getGlobal().getActiveProject());
-		
-		fr.add(tble);
-		fr.repaint();
-		
-
-		
-		
-
+		System.out.println(Global.getGlobal().getActiveProject().toString());
+*/		
 	}
 }
