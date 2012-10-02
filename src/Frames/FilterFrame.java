cd200db051818ab9903cd9051ae00c6b864db2ba
Clara <Clara@Clara-PC>
2012-10-02 15:24:21 +0200

    Imports added (Image)

--- src/Frames/FilterFrame.java
+++ src/Frames/FilterFrame.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
