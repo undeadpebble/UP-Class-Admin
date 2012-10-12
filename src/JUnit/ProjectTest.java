24426a9ea0115158cd20a0c2d090537ddc6b02d3
WilhelmTruter <wilhelmtruter@gmail.com>
2012-10-12 12:34:23 +0200

    UnitTesting

---
+++ src/JUnit/ProjectTest.java
@@ -0,0 +1,35 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import ClassAdminBackEnd.FileHandler;
+import ClassAdminBackEnd.Global;
+import ClassAdminBackEnd.Project;
+import ClassAdminBackEnd.SuperEntity;
+import ClassAdminBackEnd.UnsupportedFileTypeException;
+import ClassAdminFrontEnd.HistogramFrame;
+
+public class ProjectTest {
+
+	@Test
+	public void test() throws UnsupportedFileTypeException {
+		FileHandler fileHandler;
+		fileHandler = FileHandler.get();
+		Project p = new Project();
+		Global.getGlobal().addProject(p);
+		fileHandler.openFile("test.csv", Global.getGlobal().getActiveProject());
+
+		Global.getGlobal().getActiveProject().addhistogramcharts(new HistogramFrame(Global.getGlobal().getActiveProject()));
+		Global.getGlobal().getActiveProject().incHistogramcount();
+		
+		assertEquals(1, Global.getGlobal().getActiveProject().getHistogramcount());
+		p.setFileName("test.csv");
+		assertEquals("test.csv", Global.getGlobal().getActiveProject().getFileName());
+		
+	}
+
+}
