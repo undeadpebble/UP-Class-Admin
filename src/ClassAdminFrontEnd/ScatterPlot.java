80d1adb7fe2e622e5d64300ff9ff698e2c94cb23
WilhelmTruter <wilhelmtruter@gmail.com>
2012-10-14 11:15:59 +0200

    Cleared system.out

--- src/ClassAdminFrontEnd/ScatterPlot.java
+++ src/ClassAdminFrontEnd/ScatterPlot.java
@@ -44,135 +44,121 @@ import org.jfree.ui.RefineryUtilities;
 import ClassAdminBackEnd.Global;
 import ClassAdminBackEnd.Project;
 
-
-
 public class ScatterPlot {
 
-	private ChartPanel chartPanel ;
-	private JFreeChart chart; 
+	private ChartPanel chartPanel;
+	private JFreeChart chart;
 	private XYPlot plot;
 	private XYDataset datasetMain;
 	private int[] scatterarray;
 	private Project project;
 	private ArrayList annons;
-	//Constructor
-	public ScatterPlot(Project project)
-	{
+
+	// Constructor
+	public ScatterPlot(Project project) {
 		this.project = project;
 		project.updatecharts();
-		
-		System.out.println("CREAETED ");
+		annons = new ArrayList();
 	}
-	
-	//Set the dataset of the chart
-	public void setDatasetmain(XYDataset x)
-	{
-	
+
+	// Set the dataset of the chart
+	public void setDatasetmain(XYDataset x) {
+
 		datasetMain = x;
 	}
-	
-	//Create the create the scatterplot
-	public JFreeChart createScatter(String title,final XYDataset chartdata,String xas,String yas)
-	{
-		chart = ChartFactory.createScatterPlot(
-				"Scatter Plot", xas, yas, chartdata,
-				PlotOrientation.VERTICAL, false, false, false);
+
+	// Create the create the scatterplot
+	public JFreeChart createScatter(String title, final XYDataset chartdata, String xas, String yas) {
+		chart = ChartFactory.createScatterPlot("Scatter Plot", xas, yas, chartdata, PlotOrientation.VERTICAL, false, false, false);
 		datasetMain = chartdata;
 		NumberAxis domainAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
 		domainAxis.setAutoRangeIncludesZero(false);
 		XYPlot plot = (XYPlot) chart.getPlot();
-		
+
 		XYItemRenderer renderer = plot.getRenderer();
 		renderer.setSeriesPaint(0, new Color(0xFF8400));
 		return chart;
-	}	
-	
-	//Update all the selected values of the scatterplot
+	}
+
+	// Update all the selected values of the scatterplot
 	public void updateSelectedvalues() {
 
-chart.getXYPlot().clearAnnotations();
+		chart.getXYPlot().clearAnnotations();
 
-ArrayList u = project.getSelectedIndexes();
-//Setup bordercase on charts
-if (annons.size() > 0)
-for (int w = 0; w < annons.size(); w++)
-chart.getXYPlot().addAnnotation((XYAnnotation) annons.get(w));
+		ArrayList u = project.getSelectedIndexes();
+		// Setup bordercase on charts
+		if (annons.size() > 0)
+			for (int w = 0; w < annons.size(); w++)
+				chart.getXYPlot().addAnnotation((XYAnnotation) annons.get(w));
 
-final CircleDrawer cd = new CircleDrawer(Color.red, new BasicStroke(1.0f), null);
+		final CircleDrawer cd = new CircleDrawer(Color.red, new BasicStroke(1.0f), null);
 
-for (int x = 0; x < u.size(); x++) {
-final XYAnnotation selectPlots = new XYDrawableAnnotation(datasetMain.getXValue(0, scatterarray[(Integer) u.get(x)]),
-datasetMain.getYValue(0, scatterarray[(Integer) u.get(x)]), 11, 11, cd);
+		for (int x = 0; x < u.size(); x++) {
+			final XYAnnotation selectPlots = new XYDrawableAnnotation(datasetMain.getXValue(0, scatterarray[(Integer) u.get(x)]),
+					datasetMain.getYValue(0, scatterarray[(Integer) u.get(x)]), 11, 11, cd);
 
-chart.getXYPlot().addAnnotation(selectPlots);
+			chart.getXYPlot().addAnnotation(selectPlots);
 
-}
-}
-	//Put the chart on the chartpanel
-	public ChartPanel createPanel()
-	{
-		
+		}
+	}
+
+	// Put the chart on the chartpanel
+	public ChartPanel createPanel() {
+
 		chartPanel = new ChartPanel(chart);
 		plot = chart.getXYPlot();
 		updateSelectedvalues();
-		
-		
-			chartPanel.addChartMouseListener(new ChartMouseListener() {
 
-				public void chartMouseClicked(ChartMouseEvent e) {
-					
-					MouseEvent me = e.getTrigger();
-					
-				
-					if(me.isShiftDown() == false)
-					{
+		chartPanel.addChartMouseListener(new ChartMouseListener() {
+
+			public void chartMouseClicked(ChartMouseEvent e) {
+
+				MouseEvent me = e.getTrigger();
+
+				if (me.isShiftDown() == false) {
 					chart.getXYPlot().clearAnnotations();
 					project.clearselected();
-					}
-					ChartEntity entity = ((ChartMouseEvent) e).getEntity();
-				
-					if (entity instanceof XYItemEntity && entity != null) {
-						
-						XYItemEntity ent = (XYItemEntity) entity;
-						int [] scatterwaardes = project.getScatterIndexes();
-						
-						
-						int sindex = ent.getSeriesIndex();
-						int iindex = ent.getItem();
-						System.out.println("Selected "+ datasetMain.getXValue(sindex, iindex)+"  " + datasetMain.getYValue(sindex,iindex));
-						for(int q=0;q<scatterarray.length;q++)
-						{
-							if(scatterarray[q]== iindex)
-							{
-								project.setSelected(q,true);
-								
-							}
+				}
+				ChartEntity entity = ((ChartMouseEvent) e).getEntity();
+
+				if (entity instanceof XYItemEntity && entity != null) {
+
+					XYItemEntity ent = (XYItemEntity) entity;
+					int[] scatterwaardes = project.getScatterIndexes();
+
+					int sindex = ent.getSeriesIndex();
+					int iindex = ent.getItem();
+					
+					for (int q = 0; q < scatterarray.length; q++) {
+						if (scatterarray[q] == iindex) {
+							project.setSelected(q, true);
+
 						}
-						System.out.println("Ek het klaar geset");
-						
 					}
-
-				}
-
-				@Override
-				public void chartMouseMoved(ChartMouseEvent arg0) {
-					// TODO Auto-generated method stub
-
-				}
 				
-			});
+
+				}
+
+			}
+
+			@Override
+			public void chartMouseMoved(ChartMouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
 
 		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
 		return chartPanel;
 	}
-	public void setScatterArray(int[] x)
-	{
+
+	public void setScatterArray(int[] x) {
 		scatterarray = x;
 	}
-	    
-	public void setAnnons(XYLineAnnotation x) {
-annons.add(x);
-}
 
+	public void setAnnons(XYLineAnnotation x) {
+		annons.add(x);
+	}
 
 }
